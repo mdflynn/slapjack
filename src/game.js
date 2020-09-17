445d28f6355ef852454bc9f2c6444a3ab35d5276
mdflynn <mdflynn34@outlook.com>
2020-09-17 16:53:02 -0600

    Add basic game outline to build psuedocode on

---
+++ src/game.js
@@ -0,0 +1,26 @@
+class Game {
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.cardDeck = []; //53 cards (1 wild)
+    this.centralPile = [];
+    this.turn = false;
+    this.winCount = 0;
+  }
+
+  shuffle() {
+
+  }
+
+  deal() {
+
+  }
+
+  slap() {
+
+  }
+
+  reset() {
+
+  }
+}
