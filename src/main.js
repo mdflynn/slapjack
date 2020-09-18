c9fe0de4df5617ff86eb079d946b7a1a087f24b2
mdflynn <mdflynn34@outlook.com>
2020-09-18 13:50:07 -0600

    Edit pseudocode

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-//const game = new Game()
-// const hook up to dom  'queyselcto'
-
+// const game = new Game()
+// const hook up to dom  'queyselctor'
+//
 // domEle.on('click', function(event) {
-//   game.handlePlayeClick(event)
+//   game.handlePlayerClick(event)
 // })
