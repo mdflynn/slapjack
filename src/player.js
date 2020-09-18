14396c025c58efb50fa000ac4a7d910b614652a4
mdflynn <mdflynn34@outlook.com>
2020-09-18 11:41:12 -0600

    Remove pseudocode

--- src/player.js
+++ src/player.js
@@ -2,11 +2,11 @@ class Player {
   constructor(id, wins, hand) {
     this.id = id;
     this.wins = 0;
-    this.hand = hand || [];
+    this.hand = [];
   }
 
   playCard() {
-    //take last card in array to push to centralPile in game.js
+    //take last card in array to push to top of centralPile in game.js
   }
 
   saveWinsToStorage() {
