ae3fba84f98bf872540a7d4d2528aaadbff40bd8
mdflynn <mdflynn34@outlook.com>
2020-09-17 18:18:30 -0600

    Add basic pseudocode for player class

--- src/player.js
+++ src/player.js
@@ -1,15 +1,16 @@
 class Player {
   constructor(id, wins, hand) {
     this.id = id;
-    this.wins = wins;
-    this.hand = [];
+    this.wins = 0;
+    this.hand = hand || [];
   }
 
   playCard() {
-
+    //take last card in array to push to centralPile in game.js
   }
-  
+
   saveWinsToStorage() {
 
   }
 }
+//how to push from game class to player class?
