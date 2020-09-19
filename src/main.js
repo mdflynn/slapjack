b29228da2fb22a9f23156ef98cc0a7496a9432b6
mdflynn <mdflynn34@outlook.com>
2020-09-19 16:18:23 -0600

    Add game shuffle and deal to begin game on page load function

--- src/main.js
+++ src/main.js
@@ -6,28 +6,35 @@
 // })
 var game;
 
-document.addEventListener('keydown', playerDeal)
-window.onload = startNewGame;
+document.addEventListener('keydown', buttonHandler)
+window.onload = beginGame;
 
-function startNewGame() {
+function beginGame() {
   game = new Game();
   game.createPlayer();
+  game.shuffle(game.cardDeck);
+  game.deal(game.player1, game.player2);
+}
+
+function buttonHandler(event) {
+  playerDeal(event);
+  playerSlap(event);
 }
 
 function playerDeal(event) {
-  if (event.keyCode === 'q' && game.player1.turn) {
+  if (event.key === 'q' && game.player1.turn) {
     game.player1.playCard(game);
     game.player1.turn = game.player2.turn;
-  } else if (event.keyCode === 'p' && game.player2.turn) {
+  } else if (event.key === 'p' && game.player2.turn) {
     game.player2.playCard(game);
     game.player2.turn = game.player1.turn;
   }
 }
 
 function playerSlap(event) {
-  if (event.keyCode === 'f') {
+  if (event.key === 'f') {
     game.slap(game.player1);
-  } else if (event.keyCode === 'j') {
+  } else if (event.key === 'j') {
     game.slap(game.player2);
   }
 }
