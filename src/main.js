db3278cd08575503a529ab3ff6007631830ac8c6
mdflynn <mdflynn34@outlook.com>
2020-09-19 15:36:31 -0600

    Add function listening for q and p

--- src/main.js
+++ src/main.js
@@ -6,9 +6,20 @@
 // })
 var game;
 
+document.addEventListener('keydown', playerDeal)
 window.onload = startNewGame;
 
 function startNewGame() {
   game = new Game();
   game.createPlayer();
+}
+
+function playerDeal(event) {
+  if (event.keyCode === 'q' && game.player1.turn) {
+    game.player1.playCard(game);
+    game.player1.turn = game.player2.turn;
+  } else if (event.keyCode === 'p' && game.player2.turn) {
+    game.player2.playCard(game);
+    game.player2.turn = game.player1.turn;
+  }
 }
