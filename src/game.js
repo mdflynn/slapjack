c6836d1b8d1972495a3161ee804b0f200ee4e32e
mdflynn <mdflynn34@outlook.com>
2020-09-20 16:05:34 -0600

    Correct spelling for suit

--- src/game.js
+++ src/game.js
@@ -3,14 +3,14 @@ class Game {
     this.player1 = new Player('Left');
     this.player2 = new Player('Right');
     this.cardDeck = [
-      { suite: 'red', value: 'ace', src: './assets/red-01.png'}, { suite: 'red', value: 2, src: './assets/red-02.png'}, { suite: 'red', value: 3, src: './assets/red-03.png'}, { suite: 'red', value: 4, src: './assets/red-04.png'}, { suite: 'red', value: 5, src: './assets/red-05.png'}, { suite: 'red', value: 6, src: './assets/red-06.png'}, { suite: 'red', value: 7, src: './assets/red-07.png'},
-      { suite: 'red', value: 8, src: './assets/red-08.png'}, { suite: 'red', value: 9, src: './assets/red-09.png'}, { suite: 'red', value: 10, src: './assets/red-10.png'}, { suite: 'red', value: 'jack', src: './assets/red-jack.png'}, { suite: 'red', value: 'queen', src: './assets/red-queen.png'}, { suite: 'red', value: 'king', src: './assets/red-king.png'},
-      { suite: 'blue', value: 'ace', src: './assets/blue-01.png'}, { suite: 'blue', value: 2, src: './assets/blue-02.png'}, { suite: 'blue', value: 3, src: './assets/blue-03.png'}, { suite: 'blue', value: 4, src: './assets/blue-04.png'}, { suite: 'blue', value: 5, src: './assets/blue-05.png'}, { suite: 'blue', value: 6, src: './assets/blue-06.png'}, { suite: 'blue', value: 7, src: './assets/blue-07.png'},
-      { suite: 'blue', value: 8, src: './assets/blue-08.png'}, { suite: 'blue', value: 9, src: './assets/blue-09.png'}, { suite: 'blue', value: 10, src: './assets/blue-10.png'}, { suite: 'blue', value: 'jack', src: './assets/blue-jack.png'}, { suite: 'blue', value: 'queen', src: './assets/blue-queen.png'}, { suite: 'blue', value: 'king', src: './assets/blue-king.png'},
-      { suite: 'green', value: 'ace', src: './assets/green-01.png'}, { suite: 'green', value: 2, src: './assets/green-02.png'}, { suite: 'green', value: 3, src: './assets/green-03.png'}, { suite: 'green', value: 4, src: './assets/green-04.png'}, { suite: 'green', value: 5, src: './assets/green-05.png'}, { suite: 'green', value: 6, src: './assets/green-06.png'}, { suite: 'green', value: 7, src: './assets/green-07.png'},
-      { suite: 'green', value: 8, src: './assets/green-08.png'}, { suite: 'green', value: 9, src: './assets/green-09.png'}, { suite: 'green', value: 10, src: './assets/green-10.png'}, { suite: 'green', value: 'jack', src: './assets/green-jack.png'}, { suite: 'green', value: 'queen', src: './assets/green-queen.png'}, { suite: 'green', value: 'king', src: './assets/green-king.png'},
-      { suite: 'gold', value: 'ace', src: './assets/gold-01.png'}, { suite: 'gold', value: 2, src: './assets/gold-02.png'}, { suite: 'gold', value: 3, src: './assets/gold-03.png'}, { suite: 'gold', value: 4, src: './assets/gold-04.png'}, { suite: 'gold', value: 5, src: './assets/gold-05.png'}, { suite: 'gold', value: 6, src: './assets/gold-06.png'}, { suite: 'gold', value: 7, src: './assets/gold-07.png'},
-      { suite: 'gold', value: 8, src: './assets/gold-08.png'}, { suite: 'gold', value: 9, src: './assets/gold-09.png'}, { suite: 'gold', value: 10, src: './assets/gold-10.png'}, { suite: 'gold', value: 'jack', src: './assets/gold-jack.png'}, { suite: 'gold', value: 'queen', src: './assets/gold-queen.png'}, { suite: 'gold', value: 'king', src: './assets/gold-king.png'}
+      { suit: 'red', value: 'ace', src: './assets/red-01.png'}, { suit: 'red', value: 2, src: './assets/red-02.png'}, { suit: 'red', value: 3, src: './assets/red-03.png'}, { suit: 'red', value: 4, src: './assets/red-04.png'}, { suit: 'red', value: 5, src: './assets/red-05.png'}, { suit: 'red', value: 6, src: './assets/red-06.png'}, { suit: 'red', value: 7, src: './assets/red-07.png'},
+      { suit: 'red', value: 8, src: './assets/red-08.png'}, { suit: 'red', value: 9, src: './assets/red-09.png'}, { suit: 'red', value: 10, src: './assets/red-10.png'}, { suit: 'red', value: 'jack', src: './assets/red-jack.png'}, { suit: 'red', value: 'queen', src: './assets/red-queen.png'}, { suit: 'red', value: 'king', src: './assets/red-king.png'},
+      { suit: 'blue', value: 'ace', src: './assets/blue-01.png'}, { suit: 'blue', value: 2, src: './assets/blue-02.png'}, { suit: 'blue', value: 3, src: './assets/blue-03.png'}, { suit: 'blue', value: 4, src: './assets/blue-04.png'}, { suit: 'blue', value: 5, src: './assets/blue-05.png'}, { suit: 'blue', value: 6, src: './assets/blue-06.png'}, { suit: 'blue', value: 7, src: './assets/blue-07.png'},
+      { suit: 'blue', value: 8, src: './assets/blue-08.png'}, { suit: 'blue', value: 9, src: './assets/blue-09.png'}, { suit: 'blue', value: 10, src: './assets/blue-10.png'}, { suit: 'blue', value: 'jack', src: './assets/blue-jack.png'}, { suit: 'blue', value: 'queen', src: './assets/blue-queen.png'}, { suit: 'blue', value: 'king', src: './assets/blue-king.png'},
+      { suit: 'green', value: 'ace', src: './assets/green-01.png'}, { suit: 'green', value: 2, src: './assets/green-02.png'}, { suit: 'green', value: 3, src: './assets/green-03.png'}, { suit: 'green', value: 4, src: './assets/green-04.png'}, { suit: 'green', value: 5, src: './assets/green-05.png'}, { suit: 'green', value: 6, src: './assets/green-06.png'}, { suit: 'green', value: 7, src: './assets/green-07.png'},
+      { suit: 'green', value: 8, src: './assets/green-08.png'}, { suit: 'green', value: 9, src: './assets/green-09.png'}, { suit: 'green', value: 10, src: './assets/green-10.png'}, { suit: 'green', value: 'jack', src: './assets/green-jack.png'}, { suit: 'green', value: 'queen', src: './assets/green-queen.png'}, { suit: 'green', value: 'king', src: './assets/green-king.png'},
+      { suit: 'gold', value: 'ace', src: './assets/gold-01.png'}, { suit: 'gold', value: 2, src: './assets/gold-02.png'}, { suit: 'gold', value: 3, src: './assets/gold-03.png'}, { suit: 'gold', value: 4, src: './assets/gold-04.png'}, { suit: 'gold', value: 5, src: './assets/gold-05.png'}, { suit: 'gold', value: 6, src: './assets/gold-06.png'}, { suit: 'gold', value: 7, src: './assets/gold-07.png'},
+      { suit: 'gold', value: 8, src: './assets/gold-08.png'}, { suit: 'gold', value: 9, src: './assets/gold-09.png'}, { suit: 'gold', value: 10, src: './assets/gold-10.png'}, { suit: 'gold', value: 'jack', src: './assets/gold-jack.png'}, { suit: 'gold', value: 'queen', src: './assets/gold-queen.png'}, { suit: 'gold', value: 'king', src: './assets/gold-king.png'}
     ];
     this.centralPile = [];
     this.shuffle(this.cardDeck)
@@ -37,12 +37,10 @@ class Game {
       }
     }
   }
-
+//account for slap on non card.
   slap(player) {
       if(this.slapEndGame(player)) {
         this.endGame(player);
-      } else if (this.slapSlapJack(player)) {
-        this.slapClear(player);
       } else if (this.slapSlapJack(player)) {
         this.slapClear(player);
       } else if (this.slapDouble(player)) {
@@ -69,18 +67,16 @@ class Game {
   }
 
   slapDouble(slapper) {
-    //if (this.centralPile.length > 1) {
       if (this.centralPile[0].value === this.centralPile[1].value && this.centralPile.length > 1) {
         goodSlapText(slapper, `DOUBLE`);
         return true;
-    //  }
     }
   }
 
   slapSandwich(slapper) {
     if (this.centralPile.length > 2) {
       if (this.centralPile[0].value === this.centralPile[2].value) {;
-        goodSlapText(slapper, `SANDWHICH`);
+        goodSlapText(slapper, `SANDWICH`);
         return true
       }
     }
